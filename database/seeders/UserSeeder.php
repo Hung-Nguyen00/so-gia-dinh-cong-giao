57a9dd43ef0702cc95d1c70f54e01becfa15535e
John-Nguyen0411 <thanhhung041100@gmail.com>
2021-08-30 17:43:35 +0700

    Fixed db:seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -14,30 +15,30 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->delete();
         DB::table('users')->insert([
             [
-                'ho_va_ten' => 'Administrator',
                 'email' => 'test@gmail.com',
+                'ho_va_ten' => 'Administrator',
                 'password' => bcrypt('password'),
                 'giao_phan_id' => 1,
                 'quyen_quan_tri_id' => 1,
             ],
             [
-                'ho_va_ten' => 'Agency',
                 'email' => 'testGiaoPhan@gmail.com',
+                'ho_va_ten' => 'Agency',
                 'password' => bcrypt('password'),
                 'giao_phan_id' => 1,
                 'quyen_quan_tri_id' => 2
             ],
-            [
-                'ho_va_ten' => 'End',
-                'email' => 'testGiaoXu@gmail.com',
-                'password' => bcrypt('password'),
-                'giao_phan_id' => 1,
-                'giao_xu_id' => 1,
-                'quyen_quan_tri_id' => 3
-            ]
+
+        ]);
+        User::create([
+            'email' => 'testGiaoXu@gmail.com',
+            'ho_va_ten' => 'End',
+            'password' => bcrypt('password'),
+            'giao_phan_id' => 1,
+            'giao_xu_id' => 1,
+            'quyen_quan_tri_id' => 3
         ]);
     }
 }
