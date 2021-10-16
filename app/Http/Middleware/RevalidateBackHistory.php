7993cee7e571d2e886af9d2822ab723fcae2e591
unknown <thanhhung041100@gmail.com>
2021-10-16 17:39:20 +0700

    Fixed Toast dialogue when back to previous site

--- app/Http/Middleware/RevalidateBackHistory.php
+++ app/Http/Middleware/RevalidateBackHistory.php
@@ -20,7 +20,6 @@ class RevalidateBackHistory
         $headers = [
             'Cache-Control' => 'nocache, no-store, max-age=0, must-revalidate',
             'Pragma','no-cache',
-            'Expires','Fri, 01 Jan 1990 00:00:00 GMT',
         ];
 
         foreach($headers as $key => $value) {
